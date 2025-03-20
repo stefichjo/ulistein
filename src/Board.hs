447041109ec306d5c5b64a1ee200bc0791da63f5
Stephan Schneider <stefichjo@yahoo.de>
2025-03-20 11:11:03 +0100

    use arrays

--- src/Board.hs
+++ src/Board.hs
@@ -4,21 +4,22 @@ module Board where
 import Cards (Card, rotateCard, top, left, right, bottom)
 import Data.Maybe (isJust)
 import Data.List (intercalate)
+import Data.Array (Array, array, (!), bounds, indices, elems)
 
 -- Ein Board im Aufbau, mit Maybe für leere Positionen
-type Board = [[Maybe Card]]
+type Board = Array (Int, Int) (Maybe Card)
 
 -- Ein möglicherweise ungültiges Board
 type MaybeBoard = Maybe Board
 
 -- | Zeigt ein Board mit allen vier Seiten jeder Karte an
 showBoard :: Board -> String
-showBoard board = unlines $ concatMap showBoardRow board  -- Board nicht mehr umdrehen
+showBoard board = unlines $ concatMap showBoardRow [0..2]
   where
     showBoardRow row = [
-        intercalate "  " [maybe "  _  " (\c -> "  " ++ [top c] ++ "  ") cell    | cell <- row],
-        intercalate "  " [maybe " _|_ " (\c -> " " ++ [left c] ++ "|" ++ [right c] ++ " ") cell  | cell <- row],
-        intercalate "  " [maybe "  -  " (\c -> "  " ++ [bottom c] ++ "  ") cell | cell <- row]
+        intercalate "  " [maybe "  _  " (\c -> "  " ++ [top c] ++ "  ") (board ! (row,col))    | col <- [0..2]],
+        intercalate "  " [maybe " _|_ " (\c -> " " ++ [left c] ++ "|" ++ [right c] ++ " ") (board ! (row,col))  | col <- [0..2]],
+        intercalate "  " [maybe "  -  " (\c -> "  " ++ [bottom c] ++ "  ") (board ! (row,col)) | col <- [0..2]]
         ]
 
 -- Die Reihenfolge, in der Karten platziert werden sollen:
@@ -30,8 +31,8 @@ positions = [(1,1), (1,0), (2,1), (1,2), (0,1), (2,0), (2,2), (0,0), (0,2)]
 
 -- | Zählt die Anzahl der bereits platzierten Karten
 countCards :: Board -> Int
-countCards board = length [ () | row <- board, Just _ <- row ]
+countCards board = length [() | cell <- elems board, isJust cell]
 
 -- Erstellt ein leeres 3x3 Board
 emptyBoard :: Board
-emptyBoard = replicate 3 (replicate 3 Nothing)
+emptyBoard = array ((0,0), (2,2)) [((i,j), Nothing) | i <- [0..2], j <- [0..2]]
--- test/BoardSpec.hs
+++ test/BoardSpec.hs
@@ -1,36 +1,90 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant lambda" #-}
 module BoardSpec (spec) where
 
 import Test.Hspec ( describe, it, shouldBe, Spec )
 import Board ( emptyBoard, Board )
 import Cards (Card(..))
-import Data.Maybe (isNothing)
+import Data.Maybe (isNothing, isJust)
+import Data.Array ((!), array, elems)
 
 spec :: Spec
 spec = do
   describe "Board" $ do
     it "sollte ein leeres 3x3 Board erstellen" $ do
       let board = emptyBoard
-      length board `shouldBe` 3  -- 3 Zeilen
-      all ((== 3) . length) board `shouldBe` True  -- 3 Spalten
-      all (all isNothing) board `shouldBe` True  -- Alle Felder leer
+      all isNothing (elems board) `shouldBe` True  -- Alle Felder leer
 
     it "sollte die erste Karte unrotiert in die Mitte des Boards legen" $ do
       let firstCard = Card 's' 'p' 'm' 'k'
-      let boardWithFirstCard = [
-              [Nothing, Nothing, Nothing],
-              [Nothing, Just firstCard, Nothing],
-              [Nothing, Nothing, Nothing]
+      let boardWithFirstCard = array ((0,0), (2,2)) [
+              ((0,0), Nothing), ((0,1), Nothing), ((0,2), Nothing),
+              ((1,0), Nothing), ((1,1), Just firstCard), ((1,2), Nothing),
+              ((2,0), Nothing), ((2,1), Nothing), ((2,2), Nothing)
             ]
-      (emptyBoard `with` firstCard) `shouldBe` boardWithFirstCard
+      (firstCard `on` emptyBoard) `shouldBe` boardWithFirstCard
 
-with :: Board -> Card -> Board
-with [[a0, a1, a2], [b0, _, b2], [c0, c1, c2]] card = [
-    [a0, a1, a2],
-    [b0, Just card, b2],
-    [c0, c1, c2]
+    it "sollte den nächsten Setter aus einem Board ableiten" $ do
+      let board = array ((0,0), (2,2)) [
+              ((0,0), Nothing), ((0,1), Nothing), ((0,2), Nothing),
+              ((1,0), Nothing), ((1,1), Just (Card 's' 'p' 'm' 'k')), ((1,2), Nothing),
+              ((2,0), Nothing), ((2,1), Nothing), ((2,2), Nothing)
+            ]
+      let cardsCount = length [() | Just _ <- elems board]
+      let position = positions !! cardsCount
+      let nextSetter = makeSetter position -- TODO: makeSetter :: Board -> Setter
+
+      let nextCard = Card 'S' 'P' 'M' 'K'
+      let nextBoard = array ((0,0), (2,2)) [
+              ((0,0), Nothing), ((0,1), Nothing), ((0,2), Nothing),
+              ((1,0), Just nextCard), ((1,1), Just (Card 's' 'p' 'm' 'k')), ((1,2), Nothing),
+              ((2,0), Nothing), ((2,1), Nothing), ((2,2), Nothing)
+            ]
+
+      nextSetter nextCard board `shouldBe` nextBoard
+
+    it "sollte eine Position in einen Setter umwandeln" $ do
+      let card = Card 's' 'p' 'm' 'k'
+      let board = array ((0,0), (2,2)) [
+              ((0,0), Nothing), ((0,1), Nothing), ((0,2), Nothing),
+              ((1,0), Just card), ((1,1), Nothing), ((1,2), Nothing),
+              ((2,0), Nothing), ((2,1), Nothing), ((2,2), Nothing)
+            ]
+      let setter c = \b -> array ((0,0), (2,2)) [
+              ((0,0), b ! (0,0)), ((0,1), b ! (0,1)), ((0,2), b ! (0,2)),
+              ((1,0), Just c), ((1,1), b ! (1,1)), ((1,2), b ! (1,2)),
+              ((2,0), b ! (2,0)), ((2,1), b ! (2,1)), ((2,2), b ! (2,2))
+            ]
+
+      let position = (1,0)
+
+      makeSetter position card emptyBoard `shouldBe` board
+
+nextSetter' :: Board -> Setter
+nextSetter' board =
+  let
+    cardsCount = length [() | Just _ <- elems board]
+    position = positions !! cardsCount
+  in
+    undefined
+
+on :: Card -> Board -> Board
+card `on` board = array ((0,0), (2,2)) [
+    ((0,0), board ! (0,0)), ((0,1), board ! (0,1)), ((0,2), board ! (0,2)),
+    ((1,0), board ! (1,0)), ((1,1), Just card), ((1,2), board ! (1,2)),
+    ((2,0), board ! (2,0)), ((2,1), board ! (2,1)), ((2,2), board ! (2,2))
   ]
 
--- TODO: list of selectors, instead of list of positions
--- TODO: coordinates -> selector
--- TODO: use arrays instead of lists of lists
+type Setter = Card -> Board -> Board
 
+makeSetter :: Position -> Setter
+makeSetter (1, 0) c = \b -> array ((0,0), (2,2)) [
+    ((0,0), b ! (0,0)), ((0,1), b ! (0,1)), ((0,2), b ! (0,2)),
+    ((1,0), Just c), ((1,1), b ! (1,1)), ((1,2), b ! (1,2)),
+    ((2,0), b ! (2,0)), ((2,1), b ! (2,1)), ((2,2), b ! (2,2))
+  ]
+
+type Position = (Int, Int)
+
+positions :: [Position]
+positions = [(1,1), (1,0), (2,1), (1,2), (0,1), (2,0), (2,2), (0,2), (0,0)]
