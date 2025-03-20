01373dd8a3db23098218410f133dbd0b2a628d84
Stephan Schneider <stefichjo@yahoo.de>
2025-03-20 11:45:33 +0100

    .

--- src/Board.hs
+++ src/Board.hs
@@ -4,10 +4,19 @@ module Board where
 import Cards (Card, rotateCard, top, left, right, bottom)
 import Data.Maybe (isJust)
 import Data.List (intercalate)
-import Data.Array (Array, array, (!), bounds, indices, elems)
+import Data.Array (Array, array, (!), bounds, indices, elems, (//))
+
+type Position = (Int, Int)
+
+-- Die Reihenfolge, in der Karten platziert werden sollen:
+-- 9 2 6
+-- 5 1 3
+-- 8 4 7
+positions :: [Position]
+positions = [(1,1), (1,0), (2,1), (1,2), (0,1), (2,0), (2,2), (0,2), (0,0)]
 
 -- Ein Board im Aufbau, mit Maybe für leere Positionen
-type Board = Array (Int, Int) (Maybe Card)
+type Board = Array Position (Maybe Card)
 
 -- Ein möglicherweise ungültiges Board
 type MaybeBoard = Maybe Board
@@ -22,13 +31,6 @@ showBoard board = unlines $ concatMap showBoardRow [0..2]
         intercalate "  " [maybe "  -  " (\c -> "  " ++ [bottom c] ++ "  ") (board ! (row,col)) | col <- [0..2]]
         ]
 
--- Die Reihenfolge, in der Karten platziert werden sollen:
--- 9 2 6
--- 5 1 3
--- 8 4 7
-positions :: [(Int, Int)]
-positions = [(1,1), (1,0), (2,1), (1,2), (0,1), (2,0), (2,2), (0,0), (0,2)]
-
 -- | Zählt die Anzahl der bereits platzierten Karten
 countCards :: Board -> Int
 countCards board = length [() | cell <- elems board, isJust cell]
@@ -36,3 +38,14 @@ countCards board = length [() | cell <- elems board, isJust cell]
 -- Erstellt ein leeres 3x3 Board
 emptyBoard :: Board
 emptyBoard = array ((0,0), (2,2)) [((i,j), Nothing) | i <- [0..2], j <- [0..2]]
+
+on :: Card -> Board -> Board
+card `on` board =
+  let
+    cardsCount = length $ filter isJust (elems board)
+    setter card b = b // [(positions !! cardsCount, Just card)]
+  in
+    setter card board
+
+type Setter = Card -> Board -> Board
+
--- test/BoardSpec.hs
+++ test/BoardSpec.hs
@@ -3,10 +3,10 @@
 module BoardSpec (spec) where
 
 import Test.Hspec ( describe, it, shouldBe, Spec )
-import Board ( emptyBoard, Board )
 import Cards (Card(..))
 import Data.Maybe (isNothing, isJust)
-import Data.Array ((!), array, elems)
+import Data.Array ((!), array, elems, (//))
+import Board
 
 spec :: Spec
 spec = do
@@ -24,67 +24,16 @@ spec = do
             ]
       (firstCard `on` emptyBoard) `shouldBe` boardWithFirstCard
 
-    it "sollte den nächsten Setter aus einem Board ableiten" $ do
-      let board = array ((0,0), (2,2)) [
+    it "sollte die erste Karte auf einem (leeren) Board platzieren" $ do
+      let firstCard = Card 's' 'p' 'm' 'k'
+      let boardWithFirstCard = array ((0,0), (2,2)) [
               ((0,0), Nothing), ((0,1), Nothing), ((0,2), Nothing),
-              ((1,0), Nothing), ((1,1), Just (Card 's' 'p' 'm' 'k')), ((1,2), Nothing),
+              ((1,0), Nothing), ((1,1), Just firstCard), ((1,2), Nothing),
               ((2,0), Nothing), ((2,1), Nothing), ((2,2), Nothing)
             ]
-      let cardsCount = length [() | Just _ <- elems board]
-      let position = positions !! cardsCount
-      let nextSetter = makeSetter position -- TODO: makeSetter :: Board -> Setter
+      (firstCard `on` emptyBoard) `shouldBe` boardWithFirstCard
 
+    it "sollte die zweite Karte auf dem Board platzieren" $ do
+      let board = Card 's' 'p' 'm' 'k' `on` emptyBoard
       let nextCard = Card 'S' 'P' 'M' 'K'
-      let nextBoard = array ((0,0), (2,2)) [
-              ((0,0), Nothing), ((0,1), Nothing), ((0,2), Nothing),
-              ((1,0), Just nextCard), ((1,1), Just (Card 's' 'p' 'm' 'k')), ((1,2), Nothing),
-              ((2,0), Nothing), ((2,1), Nothing), ((2,2), Nothing)
-            ]
-
-      nextSetter nextCard board `shouldBe` nextBoard
-
-    it "sollte eine Position in einen Setter umwandeln" $ do
-      let card = Card 's' 'p' 'm' 'k'
-      let board = array ((0,0), (2,2)) [
-              ((0,0), Nothing), ((0,1), Nothing), ((0,2), Nothing),
-              ((1,0), Just card), ((1,1), Nothing), ((1,2), Nothing),
-              ((2,0), Nothing), ((2,1), Nothing), ((2,2), Nothing)
-            ]
-      let setter c = \b -> array ((0,0), (2,2)) [
-              ((0,0), b ! (0,0)), ((0,1), b ! (0,1)), ((0,2), b ! (0,2)),
-              ((1,0), Just c), ((1,1), b ! (1,1)), ((1,2), b ! (1,2)),
-              ((2,0), b ! (2,0)), ((2,1), b ! (2,1)), ((2,2), b ! (2,2))
-            ]
-
-      let position = (1,0)
-
-      makeSetter position card emptyBoard `shouldBe` board
-
-nextSetter' :: Board -> Setter
-nextSetter' board =
-  let
-    cardsCount = length [() | Just _ <- elems board]
-    position = positions !! cardsCount
-  in
-    undefined
-
-on :: Card -> Board -> Board
-card `on` board = array ((0,0), (2,2)) [
-    ((0,0), board ! (0,0)), ((0,1), board ! (0,1)), ((0,2), board ! (0,2)),
-    ((1,0), board ! (1,0)), ((1,1), Just card), ((1,2), board ! (1,2)),
-    ((2,0), board ! (2,0)), ((2,1), board ! (2,1)), ((2,2), board ! (2,2))
-  ]
-
-type Setter = Card -> Board -> Board
-
-makeSetter :: Position -> Setter
-makeSetter (1, 0) c = \b -> array ((0,0), (2,2)) [
-    ((0,0), b ! (0,0)), ((0,1), b ! (0,1)), ((0,2), b ! (0,2)),
-    ((1,0), Just c), ((1,1), b ! (1,1)), ((1,2), b ! (1,2)),
-    ((2,0), b ! (2,0)), ((2,1), b ! (2,1)), ((2,2), b ! (2,2))
-  ]
-
-type Position = (Int, Int)
-
-positions :: [Position]
-positions = [(1,1), (1,0), (2,1), (1,2), (0,1), (2,0), (2,2), (0,2), (0,0)]
+      nextCard `on` board `shouldBe` board // [((1, 0), Just nextCard)]
